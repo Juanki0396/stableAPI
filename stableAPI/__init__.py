a11825410add0017341ee15616dcecf16d5f57a8
Juanki0396 <juanki0396@gmail.com>
2022-11-15 01:08:32 +0100

    Starting to work on the front end

--- stableAPI/__init__.py
+++ stableAPI/__init__.py
@@ -1,3 +1,18 @@
 """Example docstring."""
 
 __version__ = "0.1.0"
+
+import typing
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/hello/")
+@app.route("/hello/<string:name>")
+def hello_world(name: typing.Optional[str] = None) -> str:
+    """Do the welcome to new users."""
+    if name is None:
+        name = "Bro"
+    return flask.render_template("hello.html", name=name)
